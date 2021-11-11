f165fdefcf4439f5f932cf1cd8ca060fc45d8bee
viniolimpio3 <viniolimpio3@gmail.com>
2021-11-10 23:08:01 -0300

    inicial - lógica de tiros e spawn de npcs

---
+++ Jogao N2/FrmRanking.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Jogao_N2
+{
+    partial class FrmRanking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lbRanking = new System.Windows.Forms.ListBox();
+            this.btnTelaInicial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Berlin Sans FB", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(333, 35);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(128, 35);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Ranking";
+            // 
+            // lbRanking
+            // 
+            this.lbRanking.FormattingEnabled = true;
+            this.lbRanking.ItemHeight = 18;
+            this.lbRanking.Location = new System.Drawing.Point(12, 100);
+            this.lbRanking.Name = "lbRanking";
+            this.lbRanking.Size = new System.Drawing.Size(749, 382);
+            this.lbRanking.TabIndex = 1;
+            // 
+            // btnTelaInicial
+            // 
+            this.btnTelaInicial.Location = new System.Drawing.Point(328, 511);
+            this.btnTelaInicial.Name = "btnTelaInicial";
+            this.btnTelaInicial.Size = new System.Drawing.Size(133, 56);
+            this.btnTelaInicial.TabIndex = 2;
+            this.btnTelaInicial.Text = "Voltar ao Início";
+            this.btnTelaInicial.UseVisualStyleBackColor = true;
+            this.btnTelaInicial.Click += new System.EventHandler(this.btnTelaInicial_Click);
+            // 
+            // FrmRanking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 591);
+            this.Controls.Add(this.btnTelaInicial);
+            this.Controls.Add(this.lbRanking);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmRanking";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmRecorde";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox lbRanking;
+        private System.Windows.Forms.Button btnTelaInicial;
+    }
+}
